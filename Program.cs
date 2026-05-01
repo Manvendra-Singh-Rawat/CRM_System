cfd94a59631a2dbcfa75d26e80cd2bf13553cd3a
Manvendra Singh Rawat <manvendrasingh.r11@gmail.com>
2026-05-01 08:46:36 +0530

    Docker support

--- Program.cs
+++ Program.cs
@@ -72,4 +72,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ClientManagementDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
